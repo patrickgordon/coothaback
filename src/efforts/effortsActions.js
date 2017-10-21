88abe23496efdfceef205ee15e282257c3176acf
patrick <pgord1@gmail.com>
2017-10-21 18:40:26 +1000

    Forgot to filter to auth'd user for efforts

--- src/efforts/effortsActions.js
+++ src/efforts/effortsActions.js
@@ -11,10 +11,14 @@ export const fetchEfforts = () => (dispatch, getState) => {
 	const isCacheValid = checkCacheValid(getState, "efforts");
 	if (isCacheValid) { return null; }
 
+	const state = getState();
+	const accessToken = state.authentication && state.authentication.accessToken;
+	const athleteId = state.entities.authentication[accessToken].athlete;
+
 	return dispatch({
 		[CALL_API]: {
 			types: [FETCH_REQUEST, FETCH_SUCCESS, FETCH_FAILURE],
-			endpoint: "/segments/2660310/all_efforts",
+			endpoint: `/segments/2660310/all_efforts?athlete_id=${athleteId}`,
 			method: "GET",
 			schema: effortList
 		}
