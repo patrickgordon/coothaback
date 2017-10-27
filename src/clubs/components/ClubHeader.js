e7bedc1487d2b26462b7e7cb48bef942634ad02d
patrick <pgord1@gmail.com>
2017-10-27 20:59:04 +1000

    misc style changes to card and remove console log

--- src/clubs/components/ClubHeader.js
+++ src/clubs/components/ClubHeader.js
@@ -3,7 +3,6 @@ import React from "react";
 import styles from "./ClubHeader.css";
 
 const ClubHeader = ({ imageSrc }) => {
-	console.log("imageSrc: ", imageSrc);
 	return (
 		<div className={styles.root}>
 			<img className={styles.profile} src={imageSrc} alt="club profile" />
